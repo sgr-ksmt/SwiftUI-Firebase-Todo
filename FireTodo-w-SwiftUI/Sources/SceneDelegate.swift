11348c709b54485b396d0b5f024c4471fd4bae6b
fromkk <info@fromkk.info>
2019-06-16 21:31:18 +0900

    Ready for multiple window on iPadOS

--- FireTodo-w-SwiftUI/Sources/SceneDelegate.swift
+++ FireTodo-w-SwiftUI/Sources/SceneDelegate.swift
@@ -11,7 +11,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        let window = UIWindow(frame: UIScreen.main.bounds)
+        let window = UIWindow(windowScene: scene as! UIWindowScene)
         window.rootViewController = UIHostingController(
             rootView: RootView().environmentObject(UserData())
         )
